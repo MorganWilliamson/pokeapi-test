4276b06696d286b9bd2064e4683c15a3ccc4c8c4
MorganWilliamson <mwilliamson7695@gmail.com>
2020-10-10 15:39:02 -0500

    Setting up basic state and get request.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
 
 function App() {
+
+  const [pokeData, setPokeData] = useState(null)
+  
+  
+  useEffect(() => {
+        axios.get('url')
+          .then((res) => {
+            setPokeData(res.data)
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+      }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
